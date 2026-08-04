0deadbafa04d70e603ac78977b4fb634f00d5d4b
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-08-04 12:57:55 -0700

    Use serialization support in manifest

--- app/src/main/java/com/holfuy/configtool/firmware/FirmwareManifest.kt
+++ app/src/main/java/com/holfuy/configtool/firmware/FirmwareManifest.kt
@@ -1,9 +1,13 @@
 package com.holfuy.configtool.firmware
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FirmwareManifest(
     val firmwares: List<FirmwareDescriptor>
 )
 
+@Serializable
 data class FirmwareDescriptor(
     val path: String,
     val modem: String
