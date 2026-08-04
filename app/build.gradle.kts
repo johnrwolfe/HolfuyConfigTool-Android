a571bdbd3a7a6b81e93213fd1e2db4d37ca03909
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-08-04 13:08:36 -0700

    Add HTTP support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.compose.markdown)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
