fd01c192b2cbeb8b687e8cd44bc4a247ea58769f
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-08-04 13:10:56 -0700

    Download manifest

--- app/src/main/java/com/holfuy/configtool/firmware/FirmwareManager.kt
+++ app/src/main/java/com/holfuy/configtool/firmware/FirmwareManager.kt
@@ -5,8 +5,12 @@ import android.net.Uri
 import android.util.Log
 import androidx.documentfile.provider.DocumentFile
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 private val json = Json {ignoreUnknownKeys = true}
+private val client = OkHttpClient()
+private const val MANIFEST_URL = "https://holfuy.com/support/firmwares/mobile_upgrader_manifest.json"
 
 class FirmwareManager(
     private val context: Context,
@@ -17,6 +21,25 @@ class FirmwareManager(
         private const val TAG = "HolfuyUSB-FW"
     }
 
+    private fun downloadManifest(): String
+    {
+        val request =
+            Request.Builder()
+                .url(MANIFEST_URL)
+                .build()
+    
+        client.newCall(request)
+            .execute()
+            .use { response ->
+    
+                check(response.isSuccessful) {
+                    "HTTP ${response.code}"
+                }
+    
+                return response.body!!.string()
+            }
+    }
+    
     private fun parseManifest(
         text: String
     ): FirmwareManifest = json.decodeFromString(text)
